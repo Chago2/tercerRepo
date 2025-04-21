fb4da9384adde8ed891f980b511f1c8b1ff83c87
MauDev <mauriciovaldesf@hotmail.com>
2025-04-21 17:17:56 -0600

    El primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup( 
+    name="tercerRepo",
+    version="0.1.0",
+    packages= find_packages(),
+    author="Santiago Valdés",
+    author_email="mauriciovaldesf@hotmai.com",
+    url="https://github.com/Chago2",
+    description="Tercer repositorio de prueba para el curso de GitHub",
+    )
